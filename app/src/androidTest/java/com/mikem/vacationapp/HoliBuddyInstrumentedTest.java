dda6beef6469bb2f2d2217f1a00df2c35821a9c2
M4K4TT4CK <mikemcmillin@gmail.com>
2023-12-15 13:36:45 -0500

    Add HoliBuddyInstrumentedTest to androidTest

--- app/src/androidTest/java/com/mikem/vacationapp/HoliBuddyInstrumentedTest.java
+++ app/src/androidTest/java/com/mikem/vacationapp/HoliBuddyInstrumentedTest.java
@@ -13,14 +13,20 @@ import static org.junit.Assert.*;
 /**
  * Instrumented test, which will execute on an Android device.
  *
+ * This test is designed to verify that the application context is correctly initialized within the app.
+ * It retrieves the application context using InstrumentationRegistry and asserts that the package name
+ * is the expected one. This is a fundamental test to ensure that the app's basic environment is set up correctly.
+ *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class HoliBuddyInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        // Asserts that the retrieved context's package name is correct.
         assertEquals("com.mikem.vacationapp", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/mikem/vacationapp/UI/VacationList.java
+++ app/src/main/java/com/mikem/vacationapp/UI/VacationList.java
@@ -111,8 +111,6 @@ public class VacationList extends AppCompatActivity {
         } else if (id == R.id.export_to_csv) {
             createFile();
             return true;
-        } else if (id==R.id.export_to_pdf){
-            return true;
         }
         return super.onOptionsItemSelected(item);
     }
